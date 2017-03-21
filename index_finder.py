9ace31fa2d32ec74baaf5a2b6c13d4c4aade6fad
varmakosuri <varmakbsk@gmail.com>
2017-03-21 05:41:11 +0530

    Add files via upload

---
+++ index_finder.py
@@ -0,0 +1,6 @@
+user_actual_input=raw_input("enter the main string")
+user_substring_input=raw_input("enter the string")
+user_actual_input_lower=user_actual_input.lower()
+user_substring_input_lower=user_substring_input.lower()
+print "index is at location ",user_actual_input_lower.index(user_substring_input_lower)
+
